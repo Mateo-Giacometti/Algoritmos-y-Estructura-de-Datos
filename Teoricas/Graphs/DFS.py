5c16a17d84fc26ab4fdd926db70fc733841ff691
Mateo-Giacometti <mgiacometti@udesa.edu.ar>
2023-06-23 14:43:30 -0300

    Changes

---
+++ Teoricas/Graphs/DFS.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+def DFS(graph, start):
+    stack = deque()
+    visited = set()
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    graph = {'A': ['B', 'C', 'D']}
+    
+if __name__ == '__main__':
+    main()
